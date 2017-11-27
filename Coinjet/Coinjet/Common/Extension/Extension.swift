0e883d4401c256eeda117553d8b550d959ae46d4
Daeyun Ethan Kim <nicebuvr@naver.com>
2017-11-27 20:44:27 +0900

    iOS 프로젝트 생성

---
+++ Coinjet/Coinjet/Common/Extension/Extension.swift
@@ -0,0 +1,9 @@
+//
+//  Extension.swift
+//  Coinjet
+//
+//  Created by Daeyun Ethan Kim on 27/11/2017.
+//  Copyright © 2017 Daeyun Ethan Kim. All rights reserved.
+//
+
+import Foundation
---
+++ Coinjet/Coinjet/Common/Manager/KakaoManager/KakaoManager.swift
@@ -0,0 +1,9 @@
+//
+//  KakaoManager.swift
+//  Coinjet
+//
+//  Created by Daeyun Ethan Kim on 27/11/2017.
+//  Copyright © 2017 Daeyun Ethan Kim. All rights reserved.
+//
+
+import Foundation
