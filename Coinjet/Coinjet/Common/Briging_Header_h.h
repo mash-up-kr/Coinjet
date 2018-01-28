3a71f430abc359c62ac1e6e367a035c6f28d9ca0
Kyeongwan Kang <lk@lemonlab.co.kr>
2018-01-28 18:05:37 +0900

    Kakao SDK 추가

---
+++ Coinjet/Coinjet/Common/Briging_Header_h.h
@@ -0,0 +1,15 @@
+//
+//  Briging_Header_h.h
+//  Coinjet
+//
+//  Created by Lucas on 2018. 1. 28..
+//  Copyright © 2018년 Daeyun Ethan Kim. All rights reserved.
+//
+
+#ifndef Briging_Header_h_h
+#define Briging_Header_h_h
+
+#import <KakaoOpenSDK/KakaoOpenSDK.h>
+
+
+#endif /* Briging_Header_h_h */
